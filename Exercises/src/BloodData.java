2265e8ef2da2de22355eb559a39688ff63921972
ashleeryland <rylandashlee@gmail.com>
2016-08-27 14:13:48 +1000

    Blood data

---
+++ Exercises/src/BloodData.java
@@ -0,0 +1,33 @@
+/**
+ * Created by rylan on 27/08/2016.
+ */
+public class BloodData
+{
+    private String bloodType;
+    private String rhFactor;
+    public BloodData()
+    {
+        this("O", "+");
+    }
+    public BloodData(String bType, String rh)
+    {
+        bloodType = bType;
+        rhFactor = rh;
+    }
+    public void setBloodType(String bType)
+    {
+        bloodType = bType;
+    }
+    public String getBloodType()
+    {
+        return bloodType;
+    }
+    public void setRhFactor(String rh)
+    {
+        rhFactor = rh;
+    }
+    public String getRhFactor()
+    {
+        return rhFactor;
+    }
+}
